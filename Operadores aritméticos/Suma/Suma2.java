f86446459a08a5e6c4a0bc670ff313240618403b
victor hugo <victorhugoraher@gmail.com>
2021-03-10 10:05:11 -0600

    Update Suma2.java
    ¡Importante! 
    
    Cualquier problema de ejecución con el programa, por favor envíame un mensaje a victorhugoraher@gmail.com con los siguientes datos:
    -Fecha
    -Nombre (quien reporta)
    -Asunto 
    -Cuerpo del mensaje
    -Captura de pantalla (sugerido)
    
    Gracias.

--- Operadores aritméticos/Suma/Suma2.java
+++ Operadores aritméticos/Suma/Suma2.java
@@ -34,6 +34,6 @@ public class Suma2 {             //Se inicia la clase.
         resultado=n1+n2+n3;                             //Se asigna valor a la variable resultado.
         
         
-        System.out.println("El resultado de los tres número es: " + resultado);     //Impresión de mensaje y resultado de la resta.
+        System.out.println("El resultado de los tres número es: " + resultado);     //Impresión de mensaje y resultado de la suma.
     }
 }
